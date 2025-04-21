16152dc151229eee33b86b8aa0b66b7f201f4a4b
John Imeobong <imeobongjohn38@gmail.com>
2025-04-21 16:07:37 +0100

    Add files via upload

--- Occurence.java
+++ Occurence.java
@@ -4,24 +4,25 @@ public class Occurence {
 	public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 	
-	int number; occur = 0; largest = 0; smallest = 0; count = -1;
+	int number; occur = 0; largest = 0; smallest = 0; 
 	
 	do {
+	int count = -1;
 	System.out.print("Enter number: ");
 	number = input.nextInt();
 
 	if (number >= largest) {
-	largest == number;
+	largest = number;
 	occur = occur + 1;
 
 }	else if (number >= smallest) {
-	smallest == number;
+	smallest = number;
 }
-	
-}
-	while(number != count);
 	System.out.println("The largest number is: " + largest);
 	System.out.println("The occurrence of the number is: " + occur);
+}
+	while(number != count);
+	
 
 }
 }
