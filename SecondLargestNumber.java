2170c2640c349aad94fec28eb335aab0e252cfd3
John Imeobong <imeobongjohn38@gmail.com>
2025-04-21 19:11:44 +0100

    Java Chapter 4

---
+++ SecondLargestNumber.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SecondLargestNumber {
+	public static void main(String[] args) {
+
+	Scanner input = new Scanner(System.in);
+
+	int counter = 1;
+	int largest = 0;
+	int secLargest = 0;
+	int number;
+
+	System.out.println(" _- THE SECOND LARGEST -_");
+
+	while (counter <= 10) {
+	System.out.print("\nEnter a number: ");
+	number = input.nextInt();
+	counter++;
+
+	if (number > largest) {
+	largest = number;
+}
+	if (number < largest) {
+	secLargest = number;
+}
+
+}
+	System.out.println("\nThe largest number is: " + largest);
+	System.out.println("The second largest number is: " + secLargest);
+
+
+
+}
+
+
+}
