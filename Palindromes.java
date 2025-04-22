5ce80ae10e55109a86a0cd51913eb74b29705e40
John Imeobong <imeobongjohn38@gmail.com>
2025-04-22 08:27:08 +0100

    Add files via upload

---
+++ Palindromes.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Palindromes {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+
+	System.out.println(" _- PALINDROMS OF 5-DIGITS NUMBERS -_ ");
+
+	int num = 0;
+
+	System.out.print("Enter 5 digit numbers: ");
+	int num = input.nextInt();
+
+	if (num )
+
+
+}
+}
