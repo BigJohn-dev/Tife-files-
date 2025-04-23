e7695d412a85f85d1a53f2830bccab137775df94
John Imeobong <imeobongjohn38@gmail.com>
2025-04-23 23:28:22 +0100

    Chapter 4

---
+++ TabularOutput.java
@@ -0,0 +1,16 @@
+public class TabularOutput {
+	public static void main(String[] args) {
+
+	int n = 0;
+	for(int a = 1; a <= 1; a++) {
+	  for(int b = 1; b <= 5; b++) {
+	System.out.print("N" + (++n) + "  " + "  ");
+	
+}
+	System.out.println();
+}
+
+
+}
+
+}
