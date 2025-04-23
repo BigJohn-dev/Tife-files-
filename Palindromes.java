53d0d07097c5e1c3f25955e285ed50148699d7ff
John Imeobong <imeobongjohn38@gmail.com>
2025-04-23 13:04:51 +0100

    java Chapter

--- Palindromes.java
+++ Palindromes.java
@@ -6,12 +6,24 @@ public class Palindromes {
 
 	System.out.println(" _- PALINDROMS OF 5-DIGITS NUMBERS -_ ");
 
-	int num = 0;
+	int num;
+	int temp = num; 
+	int reverse = 0;
 
-	System.out.print("Enter 5 digit numbers: ");
-	int num = input.nextInt();
+	System.out.print("Enter digit numbers: ");
+	num = input.nextInt();
 
-	if (num )
+	while (temp > 0) {
+	int remainder = temp % 10;
+	reverse = reverse * 10 + remainder;
+	temp = temp / 10;
+}
+	if (num == reverse) {
+	System.out.println(temp + " is a parlindrome");
+
+}	else {
+	System.out.println(temp + " is not a parlindrome");
+}
 
 
 }
